999c4363711e201021a7f7c0c06cdfa5768e712c
Peter McNeely <mcneelypj@msoe.edu>
2018-10-20 12:26:57 -0500

    additional options to script

--- scripts/csvToJson.py
+++ scripts/csvToJson.py
@@ -8,14 +8,16 @@ csvFilePath = ""
 entries = []
 
 if __name__ == "__main__":
-	if (len(sys.argv) < 2):
-		print("Invalid syntax.\n  Rerun the script with <CSV_File_Path> <JSON_File_name>")
+	if (len(sys.argv) < 3):
+		print("Invalid syntax.\n  Rerun the script with <CSV_File_Path> <Lake_Name> <Site_Name> <JSON_File_name>")
 		print("  If no JSON file name is supplied the default 'temp.json' will given.")
 		quit()
 	else:
 		csvFilePath = sys.argv[1]
-		if (len(sys.argv) == 3):
-			jsonFileName = sys.argv[2]
+		lakeName = sys.argv[2]
+		siteName = sys.argv[3]
+		if (len(sys.argv) == 5):
+			jsonFileName = sys.argv[4]
 		else:
 			jsonFileName = "temp.json"
 		with open(jsonFileName, "w") as jsonFile:
@@ -23,4 +25,6 @@ if __name__ == "__main__":
 				csvReader = csv.DictReader(csvFile)
 				for row in csvReader:
 					entries.append(row)
-			jsonFile.write(json.dumps(entries, indent=4))
+					
+			data = {lakeName +"_" + siteName: entries}
+			jsonFile.write(json.dumps(data, indent=4))
