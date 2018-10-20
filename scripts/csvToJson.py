ce243facdf6bd2c7ed7facc19a24c544e2a47e01
Peter McNeely <mcneelypj@msoe.edu>
2018-10-20 12:12:23 -0500

    cleanup for the script

--- scripts/csvToJson.py
+++ scripts/csvToJson.py
@@ -5,28 +5,22 @@ import sys
 
 csvFilePath = ""
 
-
-
+entries = []
 
 if __name__ == "__main__":
 	if (len(sys.argv) < 2):
 		print("Invalid syntax.\n  Rerun the script with <CSV_File_Path> <JSON_File_name>")
+		print("  If no JSON file name is supplied the default 'temp.json' will given.")
 		quit()
 	else:
-		data = {}
 		csvFilePath = sys.argv[1]
-		with open(csvFilePath) as csvFile:
-			csvReader = csv.DictReader(csvFile)
-			for row in csvReader:
-				print(row)
-				date = row["Date (mm.dd.yyyy)"]
-				data[date] = row
-
 		if (len(sys.argv) == 3):
 			jsonFileName = sys.argv[2]
 		else:
-			jsonFileName = "test.json"
+			jsonFileName = "temp.json"
 		with open(jsonFileName, "w") as jsonFile:
-			jsonFile.write(json.dumps(data, indent=4))
-	
-		
+			with open(csvFilePath) as csvFile:
+				csvReader = csv.DictReader(csvFile)
+				for row in csvReader:
+					entries.append(row)
+			jsonFile.write(json.dumps(entries, indent=4))
