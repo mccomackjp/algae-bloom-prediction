f15439e5d14b5b0a00b5d13dfa1dfc5042c454c4
Peter McNeely <mcneelypj@msoe.edu>
2019-04-23 08:47:54 -0500

    Adding *_buckets to dicts to reduce the confusion

--- notebooks/models/scripts/data_dictionary.py
+++ notebooks/models/scripts/data_dictionary.py
@@ -8,32 +8,30 @@ Note: the inner variable does not matter but each of the dictionary keys does no
 considered. If using a lambda function you need if and an Else because Lambdas NEED to return something.
 """
 
-
 '''
 dictionary for 'PRCP' column
 '''
-rainy_dict = {'var1': lambda x: 1 if x > 0 else 0}
-
+rainy_dict = {'Rained_bucket': lambda x: 1 if x > 0 else 0}
 
 '''
 dictionary for 'Wind Speed'
 Values taken from the Beaufort scale
 '''
 
-windspeed_dict = {'var0':  lambda x: "Calm"              if x < 0.5          else None,
-                 'var1':  lambda x: "Light Air"         if 1.5 > x >= 0.5   else None,
-                 'var2':  lambda x: "Light breeze"      if 3.3 > x >= 1.5   else None,
-                 'var3':  lambda x: "Gentle Breeze"     if 5.5 > x >= 3.3   else None,
-                 'var4':  lambda x: "Moderate Breeze"   if 7.9 > x >= 5.5   else None,
-                 'var5':  lambda x: "Fresh Breeze"      if 10.7 > x >= 7.9  else None,
-                 'var6':  lambda x: "Strong Breeze"     if 13.8 > x >= 10.7 else None,
-                 'var7':  lambda x: "Near Gale"         if 17.1 > x >= 13.8 else None,
-                 'var8':  lambda x: "Gale"              if 20.7 > x >= 17.1 else None,
-                 'var9':  lambda x: "Strong Gale"       if 24.4 > x >= 20.7 else None,
-                 'var10': lambda x: "Storm"             if 28.4 > x >= 24.4 else None,
-                 'var11': lambda x: "Violent Storm"     if 32.6 > x >= 28.4 else None,
-                 'var12': lambda x: "Hurricane Force"   if x >= 32.6        else None
-                 }
+windspeed_dict = {'Calm_bucket': lambda x: "Calm" if x < 0.5 else None,
+                  'Light_Air_bucket': lambda x: "Light Air" if 1.5 > x >= 0.5 else None,
+                  'Light_breeze_bucket': lambda x: "Light breeze" if 3.3 > x >= 1.5 else None,
+                  'Gentle_Breeze_bucket': lambda x: "Gentle Breeze" if 5.5 > x >= 3.3 else None,
+                  'Moderate_Breeze_bucket': lambda x: "Moderate Breeze" if 7.9 > x >= 5.5 else None,
+                  'Fresh_Breeze_bucket': lambda x: "Fresh Breeze" if 10.7 > x >= 7.9 else None,
+                  'Strong_Breeze_bucket': lambda x: "Strong Breeze" if 13.8 > x >= 10.7 else None,
+                  'Near_Gale_bucket': lambda x: "Near Gale" if 17.1 > x >= 13.8 else None,
+                  'Gale_bucket': lambda x: "Gale" if 20.7 > x >= 17.1 else None,
+                  'Strong_Gale_bucket': lambda x: "Strong Gale" if 24.4 > x >= 20.7 else None,
+                  'Storm_bucket': lambda x: "Storm" if 28.4 > x >= 24.4 else None,
+                  'Violent_Storm_bucket': lambda x: "Violent Storm" if 32.6 > x >= 28.4 else None,
+                  'Hurricane_Force_bucket': lambda x: "Hurricane Force" if x >= 32.6 else None
+                  }
 
 '''
 Values taken from http://snowfence.umn.edu/Components/winddirectionanddegreeswithouttable3.htm
@@ -58,24 +56,24 @@ dictionary for 'Wind Direction'
 
 '''
 
-winddirection_dict = {'var1':  lambda x: "N"   if x < 11         else None,
-                     'var2':  lambda x: "NNE" if 33 > x >= 11   else None,
-                     'var3':  lambda x: "NE"  if 56 > x >= 33   else None,
-                     'var4':  lambda x: "ENE" if 78 > x >= 56   else None,
-                     'var5':  lambda x: "E"   if 101 > x >= 78  else None,
-                     'var6':  lambda x: "ESE" if 123 > x >= 101 else None,
-                     'var7':  lambda x: "SE"  if 146 > x >= 123 else None,
-                     'var8':  lambda x: "SSE" if 169 > x >= 146 else None,
-                     'var9':  lambda x: "S"   if 191 > x >= 169 else None,
-                     'var10': lambda x: "SSW" if 213 > x >= 191 else None,
-                     'var11': lambda x: "SW"  if 236 > x >= 213 else None,
-                     'var12': lambda x: "WSW" if 258 > x >= 236 else None,
-                     'var13': lambda x: "W"   if 281 > x >= 258 else None,
-                     'var14': lambda x: "WNW" if 303 > x >= 281 else None,
-                     'var15': lambda x: "NW"  if 326 > x >= 303 else None,
-                     'var16': lambda x: "NNW" if 348 > x >= 326 else None,
-                     'var17': lambda x: "N"   if x >= 348        else None
-                     }
+winddirection_dict = {'N_bucket_bottom': lambda x: "N" if x < 11 else None,
+                      'NNE_bucket': lambda x: "NNE" if 33 > x >= 11 else None,
+                      'NE_bucket': lambda x: "NE" if 56 > x >= 33 else None,
+                      'ENE_bucket': lambda x: "ENE" if 78 > x >= 56 else None,
+                      'E_bucket': lambda x: "E" if 101 > x >= 78 else None,
+                      'ESE_bucket': lambda x: "ESE" if 123 > x >= 101 else None,
+                      'SE_bucket': lambda x: "SE" if 146 > x >= 123 else None,
+                      'SSE_bucket': lambda x: "SSE" if 169 > x >= 146 else None,
+                      'S_bucket': lambda x: "S" if 191 > x >= 169 else None,
+                      'SSW_bucket': lambda x: "SSW" if 213 > x >= 191 else None,
+                      'SW_bucket': lambda x: "SW" if 236 > x >= 213 else None,
+                      'WSW_bucket': lambda x: "WSW" if 258 > x >= 236 else None,
+                      'W_bucket': lambda x: "W" if 281 > x >= 258 else None,
+                      'WNW_bucket': lambda x: "WNW" if 303 > x >= 281 else None,
+                      'NW_bucket': lambda x: "NW" if 326 > x >= 303 else None,
+                      'NNW_bucket': lambda x: "NNW" if 348 > x >= 326 else None,
+                      'N_bucket_top': lambda x: "N" if x >= 348 else None
+                      }
 
 '''
 dictionary for the amount of precipitation tha that fallen. Categories taken from 
@@ -84,57 +82,55 @@ https://en.wikipedia.org/wiki/Rain#Intensity
 units in mm
 column = 'PRCP'
 '''
-precipitation_dict = {'var1': lambda x: 'None'       if x < 0.001        else None,
-                     'var2': lambda x: 'Light'      if 2.5 > x >= 0.001 else None,
-                     'var3': lambda x: 'Moderate'   if 7.6 > x >= 2.5   else None,
-                     'var4': lambda x: 'Heavy'      if 50.0 > x >= 7.6  else None,
-                     'var5': lambda x: 'Violent'    if  x >= 50.0       else None}
+precipitation_dict = {'None_bucket': lambda x: 'None' if x < 0.001 else None,
+                      'Light_bucket': lambda x: 'Light' if 2.5 > x >= 0.001 else None,
+                      'Moderate_bucket': lambda x: 'Moderate' if 7.6 > x >= 2.5 else None,
+                      'Heavy_bucket': lambda x: 'Heavy' if 50.0 > x >= 7.6 else None,
+                      'Violent_bucket': lambda x: 'Violent' if x >= 50.0 else None}
 
 '''
 dictionary for the amount of snow that has fallen
 
 coulmn = 'SNOW'
 '''
-snow_dict = {'var1': lambda x: 'None'     if x < 0.001        else None,
-            'var2': lambda x: 'Light'    if 2.0 > x >= 0.001 else None,
-            'var3': lambda x: 'Moderate' if 5.0 > x >= 2.0   else None,
-            'var4': lambda x: 'Heavy'    if x >= 5.0         else None}
+snow_dict = {'None_bucket': lambda x: 'None' if x < 0.001 else None,
+             'Light_bucket': lambda x: 'Light' if 2.0 > x >= 0.001 else None,
+             'Moderate_bucket': lambda x: 'Moderate' if 5.0 > x >= 2.0 else None,
+             'Heavy_bucket': lambda x: 'Heavy' if x >= 5.0 else None}
 
 '''
 dictionary for the snow depth
 
 column = 'SNWD'
 '''
-snow_depth_dict = {'var1': lambda x: 'None'      if x < 0.001         else None,
-                  'var2': lambda x: 'Minimal'   if 5.0 > x >=  0.001 else None,
-                  'var3': lambda x: 'Moderate'  if 10.0 > x >= 5.0   else None,
-                  'var4': lambda x: 'Deep'      if x >= 10.0         else None}
-
+snow_depth_dict = {'None_bucket': lambda x: 'None' if x < 0.001 else None,
+                   'Minimal_bucket': lambda x: 'Minimal' if 5.0 > x >= 0.001 else None,
+                   'Moderate_bucket': lambda x: 'Moderate' if 10.0 > x >= 5.0 else None,
+                   'Deep_bucket': lambda x: 'Deep' if x >= 10.0 else None}
 
 '''
 dictionary for the maximum air temperature
 
 column = 'TMAX' or 'TMIN'
 '''
-temp_dict = {'var1': lambda x: 'Freezing'    if x < 0                else None,
-            'var2': lambda x: 'Cold'        if 4.44 > x >= 0        else None,
-            'var3': lambda x: 'Cool'        if 10.0 > x >= 4.44     else None,
-            'var4': lambda x: 'Moderate'    if 15.56 > x >= 10.0    else None,
-            'var5': lambda x: 'Room'        if 21.11 > x >=  15.56  else None,
-            'var6': lambda x: 'Warm'        if 26.67 > x >= 21.11   else None,
-            'var7': lambda x: 'Hot'         if 32.22 > x >= 26.67   else None,
-            'var8': lambda x: 'Extreme'     if 37.78 > x >= 32.22   else None,
-            'var9': lambda x: 'Dangerous'   if  x >= 37.78          else None
-            }
+temp_dict = {'Freezing_bucket': lambda x: 'Freezing' if x < 0 else None,
+             'Cold_bucket': lambda x: 'Cold' if 4.44 > x >= 0 else None,
+             'Cool_bucket': lambda x: 'Cool' if 10.0 > x >= 4.44 else None,
+             'Moderate_bucket': lambda x: 'Moderate' if 15.56 > x >= 10.0 else None,
+             'Room_bucket': lambda x: 'Room' if 21.11 > x >= 15.56 else None,
+             'Warm_bucket': lambda x: 'Warm' if 26.67 > x >= 21.11 else None,
+             'Hot_bucket': lambda x: 'Hot' if 32.22 > x >= 26.67 else None,
+             'Extreme_bucket': lambda x: 'Extreme' if 37.78 > x >= 32.22 else None,
+             'Dangerous_bucket': lambda x: 'Dangerous' if x >= 37.78 else None
+             }
 
 '''
 all of the dictionaries in one. Key = column name, value = dictionary defining categories
 '''
 all_dict = {'Wind Speed': windspeed_dict,
-           'Wind Angle': winddirection_dict,
-           'PRCP': precipitation_dict,
-           'SNOW': snow_dict,
-           'SNWD': snow_depth_dict,
-           'TMAX': temp_dict,
-           'TMIN': temp_dict}
-
+            'Wind Angle': winddirection_dict,
+            'PRCP': precipitation_dict,
+            'SNOW': snow_dict,
+            'SNWD': snow_depth_dict,
+            'TMAX': temp_dict,
+            'TMIN': temp_dict}
